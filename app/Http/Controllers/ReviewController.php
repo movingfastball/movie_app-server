c2ea06387a337ccecc0174f27ec57ca0a5e977be
movingfastball <movingfastballny2012@yahoo.co.jp>
2024-03-04 11:24:30 +0900

    9-66

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -65,7 +65,9 @@ class ReviewController extends Controller
      */
     public function show(Review $review)
     {
-        //
+        $review->load('user','comments.user');
+        
+        return response()->json($review);
     }
 
     /**
--- routes/api.php
+++ routes/api.php
@@ -23,4 +23,4 @@ Route::get('/reviews/{media_type}/{media_id}',[ReviewController::class, 'index']
 Route::post('/reviews',[ReviewController::class, 'store']);
 Route::delete('/review/{review}',[ReviewController::class, 'destroy']);
 
-Route::put('/review/{review}',[ReviewController::class, 'show']);
+Route::get('/review/{review}',[ReviewController::class, 'show']);
